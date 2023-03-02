4aaf26a5c29a4972b226477b2633aa9a4d393d44
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-02 15:34:23 -0500

    Added changes in the ec2 instnace

--- ec2.tf
+++ ec2.tf
@@ -113,19 +113,56 @@ resource "aws_security_group" "app_security_group" {
 
 
 resource "aws_instance" "ec2" {
+  depends_on    = [aws_db_instance.rds_instance, aws_s3_bucket.private_bucket]
   ami           = data.aws_ami.webapp_ami.id
   instance_type = var.ec2_class
   key_name      = var.key_pair
   subnet_id     = aws_subnet.public_subnet[0].id
 
-  user_data = <<EOF
+  #Sending User Data to EC2
+  user_data = <<EOT
 #!/bin/bash
-export USERNAME=${var.db_username}
-export PASSWORD=${var.db_password}
-export HOST=${aws_db_instance.rds_instance.endpoint}
-# export S3_BUCKET_NAME=${aws_s3_bucket.private_bucket.id}
+cat <<EOF > /etc/systemd/system/webapp.service
+[Unit]
+Description=Webapp Service
+After=network.target
+
+[Service]
+Environment="NODE_ENV=development"
+Environment="PORT=3000"
+Environment="DIALECT=postgresql"
+Environment="DB_HOST=${element(split(":", aws_db_instance.rds_instance.endpoint), 0)}"
+Environment="DB_USERNAME=${aws_db_instance.rds_instance.username}"
+Environment="DB_PASSWORD=${aws_db_instance.rds_instance.password}"
+Environment="DB_NAME=${aws_db_instance.rds_instance.db_name}"
+Environment="S3_BUCKET_NAME=${aws_s3_bucket.private_bucket.bucket}"
+Environment="AWS_REGION=${var.region}"
+
+Type=simple
+User=ec2-user
+WorkingDirectory=/home/ec2-user/webapp
+ExecStart=/usr/bin/node server-listener.js
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/webapp.service
 EOF
 
+sudo systemctl daemon-reload
+sudo systemctl start webapp.service
+sudo systemctl enable webapp.service
+
+echo 'export NODE_ENV=dev' >> /home/ec2-user/.bashrc,
+echo 'export PORT=3000' >> /home/ec2-user/.bashrc,
+echo 'export DIALECT=mysql' >> /home/ec2-user/.bashrc,
+echo 'export DB_HOST=${element(split(":", aws_db_instance.rds_instance.endpoint), 0)}' >> /home/ec2-user/.bashrc,
+echo 'export DB_USERNAME=${aws_db_instance.rds_instance.username}' >> /home/ec2-user/.bashrc,
+echo 'export DB_PASSWORD=${aws_db_instance.rds_instance.password}' >> /home/ec2-user/.bashrc,
+echo 'export DB_NAME=${aws_db_instance.rds_instance.db_name}' >> /home/ec2-user/.bashrc,
+echo 'export S3_BUCKET_NAME=${aws_s3_bucket.private_bucket.bucket}' >> /home/ec2-user/.bashrc,
+echo 'export AWS_REGION=${var.region}' >> /home/ec2-user/.bashrc,
+source /home/ec2-user/.bashrc
+EOT
 
   vpc_security_group_ids = ["${aws_security_group.app_security_group.id}"]
   root_block_device {
