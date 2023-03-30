cd8a50943645fca8ae3e51237cf7b88726bf9992
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-30 12:00:25 -0400

    Added a new cloudwatch policy for webapp that pushes the logs to cloudwatch

--- iam.tf
+++ iam.tf
@@ -26,6 +26,44 @@ resource "aws_iam_role" "ec2_role" {
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2-CSYE6225-Instance-Profile"
-
   role = aws_iam_role.ec2_role.name
+}
+
+# Create IAM policy for Cloudwatch
+resource "aws_iam_policy" "WebAppCloudWatch" {
+  name        = "WebAppCloudWatch"
+  description = "Allows EC2 instances to access CloudWatch"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "cloudwatch:PutMetricData",
+            "ec2:DescribeTags",
+            "logs:PutLogEvents",
+            "logs:DescribeLogStreams",
+            "logs:DescribeLogGroups",
+            "logs:CreateLogStream",
+            "logs:CreateLogGroup"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameter",
+            "ssm:PutParameter"
+          ],
+          "Resource" : "arn:aws:ssm:::parameter/AmazonCloudWatch-*"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy_attachment" {
+  policy_arn = aws_iam_policy.WebAppCloudWatch.arn
+  role       = aws_iam_role.ec2_role.name
 }
