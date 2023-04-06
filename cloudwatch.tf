7f5a64acb1b914246088fb5cdeaf123b684b39b4
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-06 14:41:44 -0400

    Commented the cloudwatch config to delete the logs

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,8 +1,8 @@
-resource "aws_cloudwatch_log_group" "csye" {
-  name = "csye6225"
-}
+# resource "aws_cloudwatch_log_group" "csye" {
+#   name = "csye6225"
+# }
 
-resource "aws_cloudwatch_log_stream" "webapp" {
-  name           = "webapp"
-  log_group_name = aws_cloudwatch_log_group.csye.name
-}
+# resource "aws_cloudwatch_log_stream" "webapp" {
+#   name           = "webapp"
+#   log_group_name = aws_cloudwatch_log_group.csye.name
+# }
