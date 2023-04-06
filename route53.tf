a9194aef65ca837d027369658ff3875adfd40603
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-06 14:04:31 -0400

    Updated to use aws_launch_configuration instead of aws_launch_template

--- route53.tf
+++ route53.tf
@@ -10,10 +10,10 @@ variable "dev_A_record_name" {
 variable "prod_A_record_name" {
   type = string
 }
-resource "aws_eip" "eip" {
-  instance = aws_instance.ec2.id
-  vpc      = true
-}
+# resource "aws_eip" "eip" {
+#   instance = aws_instance.ec2.id
+#   vpc      = true
+# }
 resource "aws_route53_record" "nithinbharadwaj_A_record" {
   zone_id = var.profile == "dev" ? var.dev_hostedzone_id : var.prod_hostedzone_id
   name    = var.profile == "dev" ? var.dev_A_record_name : var.prod_A_record_name
